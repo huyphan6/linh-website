a253ff85cf4b599d7f2e479f05edb8001a09b80e
Huy Phan <phan.q.huy21@gmail.com>
2024-04-25 01:57:11 -0400

    added MUI dependencies

--- src/app/page.js
+++ src/app/page.js
@@ -2,15 +2,9 @@ import React from "react";
 
 const Home = () => {
     return (
-        <div
-            display="flex"
-            justify-content="center"
-            text-align="center"
-        >
-            <div text-align="center">
-                <h1>Home</h1>
-                <p>Welcome to the Home page</p>
-            </div>
+        <div display="flex" justify-content="center" text-align="center">
+            <h1>Home</h1>
+            <p>Welcome to the Home page</p>
         </div>
     );
 };
